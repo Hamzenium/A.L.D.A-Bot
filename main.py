34a7273947376dfe1afee816374c2034c9055e45
Muhammad Hamza Sohail <67511980+Hamzenium@users.noreply.github.com>
2023-09-23 05:26:47 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,19 +13,19 @@ def home():
 
 @app.route("/submit")
 def form():
-    # str = '/Users/muhammadhamzasohail/Desktop/Tester_ALDA'
-    # str1 = '/Users/muhammadhamzasohail/Desktop/Summary_files'
-    # result = nlp.path_adder(str)
-    # nlp.batch_processor(result, str, str1)
+    str = '/Users/muhammadhamzasohail/Desktop/Tester_ALDA'
+    str1 = '/Users/muhammadhamzasohail/Desktop/Summary_files'
+    result = nlp.path_adder(str)
+    nlp.batch_processor(result, str, str1)
     return render_template('form.html')
 
 
 @app.route("/last")
 def last():
-    # str = '/Users/muhammadhamzasohail/Desktop/Tester_ALDA'
-    # str1 = '/Users/muhammadhamzasohail/Desktop/Summary_files'
-    # result = nlp.path_adder(str)
-    # nlp.batch_processor(result, str, str1)
+    str = '/Users/muhammadhamzasohail/Desktop/Tester_ALDA'
+    str1 = '/Users/muhammadhamzasohail/Desktop/Summary_files'
+    result = nlp.path_adder(str)
+    nlp.batch_processor(result, str, str1)
     return render_template('last.html')
 
 if __name__ == "__main__":
